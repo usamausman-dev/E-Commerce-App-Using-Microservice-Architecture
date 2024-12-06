78d8f717d0bf5b76e49ba73a358b4a13f7524ef2
Usama Usman <50479258+usamausman-dev@users.noreply.github.com>
2024-12-06 17:11:08 +0500

    FIX : PORTS

--- ProductService/index.js
+++ ProductService/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose');
 require('dotenv').config()
 const app = express()
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT||5002;
 app.use(express.json())
 app.use('/api', require('./products/router'))
 
--- UserService/index.js
+++ UserService/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const userRoutes = require('./userRoutes');
 require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 app.use(express.json());
 app.use('/user', userRoutes);
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
